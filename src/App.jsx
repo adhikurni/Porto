75291a1be5f02f8d7e01e0c17595f069a886f519
adhikurni <adhi.kurni@gmail.com>
2025-11-20 19:43:18 +0700

    add lanyard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ColorBends from './components/ColorBends/ColorBends.jsx'
+import Lanyard from './components/Lanyard/Lanyard.jsx'
 
 
 function App() {
@@ -12,18 +13,18 @@ function App() {
     <div className='colorbends-container'> 
       <ColorBends
         colors={["#ff0000ff", "#15ff00ff", "#2f00ffff"]}
-        rotation={-167}
-        speed={0.2}
+        rotation={-11}
+        speed={0.1}
         scale={1}
         frequency={1}
-        warpStrength={0.93
-        }
+        warpStrength={0.95}
         mouseInfluence={0.2}
         autoRotate = {1}
         parallax={0.5}
         noise={0.1}
         transparent
       />
+      <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
     </div>
   )
 }
